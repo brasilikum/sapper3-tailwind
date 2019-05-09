e361be916e4cda48fdb4a07b473cced63a7d3a9d
Georg Hartmann <mail@georg-hartmann.com>
2019-05-08 23:20:45 -0500

    initial

---
+++ src/routes/index.svelte
@@ -0,0 +1,23 @@
+<style>
+  @tailwind preflight;
+  @tailwind components;
+  @tailwind utilities;
+  @tailwind screens;
+</style>
+
+<svelte:head>
+  <title>Sapper project template</title>
+</svelte:head>
+
+<h1 class="hidden">Great success!</h1>
+
+<figure>
+  <img alt="Borat" src="great-success.png" />
+  <figcaption>HIGH FIVE!</figcaption>
+</figure>
+
+<p>
+  <strong class="">
+    Try editing this file (src/routes/index.svelte) to test live reloading.
+  </strong>
+</p>
